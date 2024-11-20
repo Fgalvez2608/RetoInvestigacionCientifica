beced60a7de3ce6a7fa6d71fdd09c2681633a463
Dilier Esteban Fortaleche Lopez <dilierestebanfortalechelopez@gmail.com>
2024-11-20 14:01:10 -0500

    correciones de errores actu1

--- main.py
+++ main.py
@@ -11,14 +11,16 @@ class Experimento:  # Clase que representa un experimento.
 
 # 1. Funcion para registrar experimento
 def registrarExperimento(listaExperimentos):  # Función que permite registrar un experimento en la lista de experimentos.
-    nombre = input('Ingrese el nombre del experimento: ')  # Variable que almacena el nombre del experimento ingresado
-    fecha_realizacion = input('Ingrese la fecha de realización del experimento (DD/MM/AAAA): ')  # Variable que almacena la fecha de realización del experimento ingresada
+    while True:
+        nombre = input('Ingrese el nombre del experimento: ')  # Variable que almacena el nombre del experimento ingresado
+        fecha_realizacion = input('Ingrese la fecha de realización del experimento (DD-MM-AAAA): ')  # Variable que almacena la fecha de realización del experimento ingresada
 
-    try:  # Manejo de error en caso de que la fecha ingresada no sea válida.
-        fechaRealizacion = datetime.strptime(fecha_realizacion, '%d/%m/%Y')
-    except ValueError:
-        print('La fecha ingresada no es válida. Por favor, ingrese una fecha válida en el formato DD/MM/AAAA.')
-        return
+        try:  # Manejo de error en caso de que la fecha ingresada no sea válida.
+            fechaRealizacion = datetime.strptime(fecha_realizacion, '%d-%m-%Y')
+            break
+        except ValueError:
+            print('La fecha ingresada no es válida. Por favor, ingrese una fecha válida en el formato DD/MM/AAAA.')
+            continue
 
     categoria = input('Ingrese la categoría del experimento (ejemplo: "Química", "Biología" o "Física"): ')  # Variable que almacena la categoría del experimento ingresada
 
@@ -26,7 +28,12 @@ def registrarExperimento(listaExperimentos):  # Función que permite registrar u
     num_resultados = int(input('Ingrese el número de resultados numéricos del experimento: '))
 
     for i in range(num_resultados):  # Iterar sobre el número de resultados numéricos ingresados y pedir al usuario que ingrese cada resultado.
-        resultado = float(input(f'Ingrese el resultado numérico {i+1}: '))
+        while True:
+            try:
+                resultado = float(input(f'Ingrese el resultado numérico {i+1}: '))
+                break
+            except ValueError:
+                print('El resultado ingresado no es un n ́umero. Por favor, ingrese un n ́umero.')
         resultados.append(resultado)  # Los resultados se almacenan en una lista.
 
     experimento = Experimento(nombre, fechaRealizacion, categoria)  # Creación de un objeto de la clase Experimento
@@ -50,7 +57,6 @@ def visualizarResultados(listaExperimentos):  # Función para visualizar resulta
         print(f' Resultados: {experimento.resultados}')  # Muestra la lista de resultados
 
 
-
 # 3. Funcion para eliminar experimento
 def eliminarExperimento(listaExperimentos):  # Función para eliminar experimento
     if not listaExperimentos:  # Verificar si la lista de experimentos está vacía.
@@ -66,6 +72,7 @@ def eliminarExperimento(listaExperimentos):  # Función para eliminar experiment
             return
 
     print(f'No se encontró el experimento {nombreExperimento}.')  # Mostrar un mensaje en caso de que no se encuentre el experimento.
+
 
 # 4. Funcion para calculo estadistico basico (PROMEDIO - VALOR MAXIMO - VALOR MINIMO).
 def analisisResultados(listaExperimentos):  # Función para cálculo estadístico básico (PROMEDIO - VALOR MÁXIMO - VALOR MÍNIMO) de los resultados numéricos.
@@ -87,35 +94,67 @@ def analisisResultados(listaExperimentos):  # Función para cálculo estadístic
 
     print(f'Resultado numérico mínimo: {minimo:.2f}')  # Imprimir el resultado numérico mínimo con 2 decimales
 
+
 # 5. Funcion para comprar 2 o mas experimentos
 def compradorExperimentos(listaExperimentos):
+    # lista para almacenar los experimentos seleccionados para comparar
     experimentos_a_comparar = []
     while True:
+        # mostrar el menú de comparación de experimentos
         print("\nSubmenú de comparación de experimentos")
         print("1. Seleccionar experimento para comparar")
-        print("2. Comparar experimentos seleccionados")
-        print("3. Salir")
-        opcion = input("Ingrese la opción: ")
-        if opcion == "1":
+        print("2. Ver experimentos seleccionados")
+        print("3. Comparar experimentos seleccionados")
+        print("4. Salir")
+        
+        # pedir al usuario que ingrese la opción deseada
+        try:
+            opcion = int(input("Ingrese la opción(entre 1 y 4): "))
+        except ValueError:
+            print("Opción no válida, ingrese un número entre 1 y 4")
+            continue
+        
+        if opcion == 1:
+            # mostrar la lista de experimentos registrados
             print("\nLista de experimentos:")
             for i, experimento in enumerate(listaExperimentos, start=1):
                 print(f"{i}. {experimento.nombre}")
-            seleccion = int(input("Ingrese el número del experimento a seleccionar: "))
+                
+            # pedir al usuario que seleccione un experimento
+            try:
+                seleccion = int(input("Ingrese el número del experimento a seleccionar: "))
+                if seleccion < 1 or seleccion > len(listaExperimentos):
+                    raise ValueError
+            except ValueError:
+                print("Selección no válida, ingrese un número de la lista")
+                continue
+            
+            # agregar el experimento seleccionado a la lista de experimentos a comparar
             experimentos_a_comparar.append(listaExperimentos[seleccion - 1])
             print(f"Experimento '{listaExperimentos[seleccion - 1].nombre}' seleccionado para comparar")
-        elif opcion == "2":
-            if len(experimentos_a_comparar) < 2:
-                print("Debe seleccionar al menos dos experimentos para comparar")
+            
+        elif opcion == 2:
+            if not experimentos_a_comparar: # verificar si hay experimentos seleccionados para comparar
+                print("No hay experimentos seleccionados para comparar")
             else:
-                # Código para comparar los experimentos seleccionados
-                print("\nComparación de experimentos:")
+                print("\nExperimentos seleccionados para comparar:") # mostrar los experimentos seleccionados para comparar
                 for i, experimento in enumerate(experimentos_a_comparar, start=1):
                     print(f"Experimento {i}: {experimento.nombre}")
-                    # Código para mostrar los resultados de la comparación
-                experimentos_a_comparar = []
-        elif opcion == "3":
+            
+        elif opcion == 3:
+            if len(experimentos_a_comparar) < 2: # verificar si hay al menos dos experimentos seleccionados para comparar
+                print("Debe seleccionar al menos dos experimentos para comparar")
+            else:
+                print("\nComparación de experimentos: ") # mostrar la comparación de los experimentos seleccionados
+                resultados = [] # crear una lista para almacenar los resultados de cada experimento
+                for experimento in experimentos_a_comparar:
+                    resultados.append(statistics.mean(experimento.resultados)) # calcular el promedio de los resultados de cada experimento
+                print(f"Promedio de resultados de los experimentos seleccionados: {statistics.mean(resultados):.2f}") # mostrar el promedio de los resultados de los experimentos seleccionados
+        elif opcion == 4:
+            # salir del menú de comparación de experimentos
             break
         else:
+            # mostrar un mensaje en caso de que la opción ingresada sea inválida
             print("Opción inválida. Por favor, ingrese una opción válida.")
 
 
@@ -134,6 +173,7 @@ def generarReporte(listaExperimentos):  # Función para generar reporte con un a
             archivo.write('\n')  # Escribir una línea en blanco para separar los experimentos.
 
     print('Informe generado en el archivo "reporte.txt" exitosamente')
+
 
 # 7. Funcion para menu principal
 def menu(listaExperimentos):  # Función para el menú principal
